d4697d442b28d0c318f625bd6912960fa6c8aa37
Beth Wilson <beth.ny.wlsn@gmail.com>
2018-07-24 22:16:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function yourAnimal() {
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
   var animal = cat;
-  return ${animal};
+  console.log(`${animal}`);
 }
 
 function add2(n) {
